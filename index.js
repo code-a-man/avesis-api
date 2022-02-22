7328401512d3738a93170b4c604948e8ce8a6a5c
Code a Man <43219246+code-a-man@users.noreply.github.com>
2022-02-22 22:25:35 +0300

    :rocket: create main function

---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+module.exports.getAvesis = async function (university, teacher) {
+    const url = `https://avesis.${university}.edu.tr/${teacher}/dokumanlar`
+	const list = []
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data)
+    $('div[class="ac-item"]').each(function(i, elem){
+		const head =  $(elem).find('div.item-head > div')
+		const body =  $(elem).find('div.item-body')
+		const linkSuffix = body.find('a.btn.btn-warning.btn-sm').attr('href')
+        list.push({
+			title : head.find('div.col-md-8.col-xs-3 > span').text().trim(),
+			type: head.find('div.col-md-2.col-xs-4 > span').text().trim(),
+			date: head.find('div.col-md-2.col-xs-5 > span').text().trim(),
+			description: body.text().trim(),
+			link: `https://avesis.${university}.edu.tr${linkSuffix}`,
+		})
+    });
+	return list
+}
+
