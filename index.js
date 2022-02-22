e216d1f62f9f71c02c03b0374799137a7235f89b
Code a Man <43219246+code-a-man@users.noreply.github.com>
2022-02-22 22:43:43 +0300

    :sparkles: error handle

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cheerio = require("cheerio")
 module.exports.getAvesis = async function (university, teacher) {
     const url = `https://avesis.${university}.edu.tr/${teacher}/dokumanlar`
 	const list = []
-    const { data } = await axios.get(url)
+    const { data } = await axios.get(url).catch(err => err);
+	if (!data) return `Couldn't reach ${url} check the url!`;
     const $ = cheerio.load(data)
     $('div[class="ac-item"]').each(function(i, elem){
 		const head =  $(elem).find('div.item-head > div')
